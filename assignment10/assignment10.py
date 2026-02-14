1bd690d047b78c62083183f55f3750b313fd4fe2
lillyp9 <lillyperez9@icloud.com>
2026-02-13 20:49:06 -0500

    Assignment10 done

---
+++ assignment10/assignment10.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import json
+#Task 1 
+#In durhamcountylibrary.org/robots.txt - All crawler indentifies (any scraping) can run except anything under /staff/ , specific named bots are not allowed in the entire site. 
+
+#Task 2 is in search-log.py 
+
+#Task 3 is in get_books.py
+
+#Task 4 in get_books.py
---
+++ assignment10/search-log.py
@@ -0,0 +1,66 @@
+#Search Results Entry:
+#first book
+<li class ="cp-search-result-item">
+#then 
+<div class ="col-md-10 item-columns">
+#then
+<div class="jacket-cover-wrap hidden-md hidden-lg" >
+class = "cp-title"
+Title: Learning Spanish-beginner I 
+<span class="cp-author-link">
+Author: Iris Acevedo A., Spanishonline Costarica
+<span class="cp-screen-reader-message">
+Date: 2025 ebook , Language: Spanish
+
+
+
+#Second book
+<li class ="row cp-search-result-item">
+#then 
+<div class="col-md-10 item-column">
+then
+<div class="jacket-cover-wwrap hidden-md hidden-lg">
+class = "cp-title"
+Title: Real-World Spanish: The Conversation Learning System 
+<span class ="cp-author-link">
+Author: Camila Vega Rivera
+<span class="cp-screen-reader-message">
+<cp-format-link>
+Date 2025 Audiobook
+
+
+
+#Third book
+<li class="row cp-search-result-item">
+#then
+<div class="cp-deprecated-bib-brief">
+
+class="cp-title"
+Title: A Beginner's Guide to Learning Spanish'
+class="cp-author-link"
+Author: Miller Jackson
+<span class="cp-screen-reader-message">
+<cp-format-link>
+Date: 2024 Audiobook
+
+#Fourth book
+<li class="row cp-search-result-item">
+#then
+<div class="cp-deprecated-bib-brief">
+Title: 100 Facts About Learning Spanish
+class="cp-subtitle"
+Subtitle: English to Spanish
+Author: Science-Based Language Learning Lab
+<span class ="display-info-primary">
+Date: 2024 ebook
+
+#Fith book 
+<li class="row cp-search-result-item">
+#then 
+<div class="col-md-10 item-column">
+<div class="cp-deprecated-bib-brief">
+<cp-title>
+Title: No Tears Spanish Grammar: Easy Learning : Essential Rules for Beginners.
+Author: Bennett,Olivia 
+
+
